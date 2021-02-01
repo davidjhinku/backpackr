6586d22432b90bd22a3d2e63c3706afce5144179
David <davidjhinku@gmail.com>
2021-02-01 14:03:44 -0500

    Added routes and components folders

---
+++ frontend/src/components/nav/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return(
+        <footer>
+            Probably not Copyright &copy; 2021 Backpackr
+        </footer>
+    )
+}
+
+export default Footer;
