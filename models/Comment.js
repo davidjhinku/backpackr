be271e47d3954482d7125cf29f8310e1fa0384a9
audreyyun <96audreyyun@gmail.com>
2021-02-01 13:07:59 -0500

    Add Schemas

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    author: {
+        _id: {
+            type: Schema.Types.ObjectId,
+            ref: 'users',
+        },
+
+        username: String
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+module.exports = Comment = mongoose.model('Comment', CommentSchema);
--- models/ItineraryItem.js
+++ models/ItineraryItem.js
@@ -2,7 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ItineraryItemSchema = new Schema({
+    _id: {
+        type: Schema.Types.ObjectId
+    },
 
+    itemName: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 
