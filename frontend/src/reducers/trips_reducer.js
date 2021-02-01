cda5105c9293f081026fa426c92655b358f2d909
David <davidjhinku@gmail.com>
2021-02-01 15:32:29 -0500

    Completed trips reducer

---
+++ frontend/src/reducers/trips_reducer.js
@@ -0,0 +1,27 @@
+import {RECEIVE_USER_TRIPS, RECEIVE_A_TRIP, RECEIVE_NEW_TRIP} from '../actions/trip_actions'
+
+const defaultState = {
+    user: {},
+    trip: {},
+    new: undefined
+}
+
+const TripsReducer = (state = defaultState, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+    switch(action.type) {
+        case RECEIVE_USER_TRIPS:
+            newState.user = action.trips.data;
+            return newState;
+        case RECEIVE_A_TRIP:
+            newState.trip = action.trip.data;
+            return newState;
+        case RECEIVE_NEW_TRIP:
+            newState.new = action.trip.data;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default TripsReducer;
