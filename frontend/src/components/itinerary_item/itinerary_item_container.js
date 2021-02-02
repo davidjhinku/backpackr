6ab5a61f8d15db198da1ea99ca51f78b6bc47ee7
audreyyun <96audreyyun@gmail.com>
2021-02-02 14:39:56 -0500

    Add Itinerary Item Show Page

---
+++ frontend/src/components/itinerary_item/itinerary_item_container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { fetchItineraryItem } from '../../actions/itinerary_item_actions';
+import ItineraryItem from './itinerary_item';
+
+
+const mSTP = (state, ownProps) => { 
+    const itemId = ownProps.match.params.itemId
+    const item = state.items.item
+    return {
+        itemId: itemId,
+        item
+    }
+}
+
+const mDTP = dispatch => { 
+    return { 
+        fetchItineraryItem: itemId => dispatch(fetchItineraryItem(itemId))
+    }
+}
+
+
+export default connect(mSTP, mDTP)(ItineraryItem)
