88dea53cd2ce9a13aead003ff80fe593bdc1ced6
audreyyun <96audreyyun@gmail.com>
2021-02-02 15:30:06 -0500

    Frontend Itinerary Item

--- frontend/src/components/itinerary_item/itinerary_item_container.js
+++ frontend/src/components/itinerary_item/itinerary_item_container.js
@@ -5,10 +5,11 @@ import ItineraryItem from './itinerary_item';
 
 const mSTP = (state, ownProps) => { 
     const itemId = ownProps.match.params.itemId
+    debugger
     const item = state.items.item
     return {
         itemId: itemId,
-        item
+        item: item
     }
 }
 
