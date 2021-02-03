e2f361e78e6789872d873e9c937d57086dc4987f
Jose Miralles <josemiralles1991@gmail.com>
2021-02-03 14:16:40 -0500

    Pulling main

---
+++ frontend/src/components/nav/footer_items/jose.js
@@ -0,0 +1,9 @@
+export default {
+    name: "Jose Miralles",
+    description: "Superdotado Superdotado Superdotado Superdotado Superdotado Superdotado Superdotado",
+    links: [
+        { name: "Personal Page", url: "http://josemiralles.github.io/" },
+        { name: "LinkedIn", url: "https://www.linkedin.com/in/josemiralles/" },
+        { name: "" }
+    ]
+}
