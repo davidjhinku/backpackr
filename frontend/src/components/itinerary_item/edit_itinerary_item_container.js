6e57bfbf31c15e9404a0141286f65256f0916148
David <davidjhinku@gmail.com>
2021-02-03 11:47:05 -0500

    Commented our edit itrms

--- frontend/src/components/itinerary_item/edit_itinerary_item_container.js
+++ frontend/src/components/itinerary_item/edit_itinerary_item_container.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { fetchItineraryItem, receiveErrors } from '../../actions/itinerary_item_actions'
+// import React from 'react'
+// import { connect } from 'react-redux';
+// import { fetchItineraryItem, receiveErrors } from '../../actions/itinerary_item_actions'
 // import { fetchItineraryItem, updateItineraryItem, receiveErrors } from '../../actions/itinerary_item_actions'
-import EditItineraryItem from './edit_itinerary_item'
+// import EditItineraryItem from './edit_itinerary_item'
 
-class EditItem extends React.Component {
-    componentDidMount() {
-        this.props.fetchItineraryItem(this.props.match.params.itemId)
-    }
+// class EditItem extends React.Component {
+//     componentDidMount() {
+//         this.props.fetchItineraryItem(this.props.match.params.itemId)
+//     }
 
-    render() {
+//     render() {
 
-        if (!this.props.item) {
-            return null;
-        } else {
-            return (
-                <EditItineraryItem
-                    item={this.props.item}
-                    // updateItineraryItem={this.props.updateItineraryItem}
-                    clearErrors={this.props.clearErrors}
-                />
-            )
-        }
-    }
-}
+//         if (!this.props.item) {
+//             return null;
+//         } else {
+//             return (
+//                 <EditItineraryItem
+//                     item={this.props.item}
+//                     // updateItineraryItem={this.props.updateItineraryItem}
+//                     clearErrors={this.props.clearErrors}
+//                 />
+//             )
+//         }
+//     }
+// }
 
 
 
-const mSTP = (state, ownProps) => {
-    return {
-        trip: state.items.item[ownProps.match.params.itemId],
-        errors: state.errors.item,
-        formType: 'Update your Itinerary Item'
-    }
-}
+// const mSTP = (state, ownProps) => {
+//     return {
+//         trip: state.items.item[ownProps.match.params.itemId],
+//         errors: state.errors.item,
+//         formType: 'Update your Itinerary Item'
+//     }
+// }
 
-const mDTP = dispatch => {
-    return {
-        fetchItineraryItem: itemId => dispatch(fetchItineraryItem(itemId)),
-        updateItineraryItem: data => dispatch(updateItineraryItem(data)),
-        clearErrors: errors => dispatch(receiveErrors(errors))
-    }
-}
+// const mDTP = dispatch => {
+//     return {
+//         fetchItineraryItem: itemId => dispatch(fetchItineraryItem(itemId)),
+//         updateItineraryItem: data => dispatch(updateItineraryItem(data)),
+//         clearErrors: errors => dispatch(receiveErrors(errors))
+//     }
+// }
 
-export default connect(mSTP, mDTP)(EditItineraryItem);
+// export default connect(mSTP, mDTP)(EditItineraryItem);
