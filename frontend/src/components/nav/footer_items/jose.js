d387e5915060f1c5950be37a7c95d6342e88352e
Jose Miralles <josemiralles1991@gmail.com>
2021-02-03 16:45:45 -0500

    Implemented initial footer.

--- frontend/src/components/nav/footer_items/jose.js
+++ frontend/src/components/nav/footer_items/jose.js
@@ -1,9 +1,9 @@
 export default {
     name: "Jose Miralles",
-    description: "Superdotado Superdotado Superdotado Superdotado Superdotado Superdotado Superdotado",
     links: [
         { name: "Personal Page", url: "http://josemiralles.github.io/" },
         { name: "LinkedIn", url: "https://www.linkedin.com/in/josemiralles/" },
-        { name: "" }
+        { name: "GitHub", url: "https://github.com/JoseMiralles" },
+        { name: "Stack Overflow", url: "https://stackoverflow.com/users/2895137/jose-miralles" }
     ]
 }
