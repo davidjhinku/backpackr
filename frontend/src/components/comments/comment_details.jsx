9774ccedd0dfa4d11804dc31fb12262227725f9a
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-02-03 16:45:44 -0600

    Add timestamp to comments

--- frontend/src/components/comments/comment_details.jsx
+++ frontend/src/components/comments/comment_details.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 
 const CommentDetails = ({ comment, deleteComment }) => {
+   
     return (
         <div className='itinerary-item-list' >
             <li>
+                {new Date(comment.date).toDateString()}
+                <br />
+                {new Date(comment.date).toLocaleTimeString()}
                 <p>{comment.comment}</p>
                 <br />
-
+                
+                <br />
                 <button onClick={() => deleteComment(comment._id)}>Delete Comment</button>
             </li>
             <br />
