b4224f1e40fdb42ac2667f56011e747ff80a1e7a
David <davidjhinku@gmail.com>
2021-02-03 16:50:44 -0500

    Working on users list

--- frontend/src/components/users_list/users_list.jsx
+++ frontend/src/components/users_list/users_list.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 class UsersList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            email: ''
+        }
+        this.inviteFriend = this.inviteFriend.bind(this)
+    }
 
-    //{`{/trips/{tripId}/edit}`} edit page
+    inviteFriend(e){
+        e.preventDefault()
+
+    }
 
     render(){
         const tripUsers = this.props.users.map((user, idx)=>{
             return (
                 <div>
-                    <h2>Adventurers</h2>
-                    <ul>
-                        <li className="trip-users-element" key={`user-${idx}`}>{user.username}</li> <Link to="">Edit</Link>
-                    </ul>
-                    <br/>
+                    <li className="trip-users-element" key={`user-${idx}`}>{user.username}</li> <Link to="">Edit</Link>
                 </div>
             )
         });
@@ -21,7 +27,22 @@ class UsersList extends React.Component{
 
         return(
             <div>
-                {tripUsers}
+                <h2>Adventurers</h2>
+                <br/>
+
+                <p>Invite you friends</p>
+                <form onSubmit={this.inviteFriend}>
+                    <input type="text"
+                        value={this.state.email}
+                        placeholder=''
+                    />
+                    <button>Send Invite</button>
+                </form>
+                <br/>
+                
+                <ul>
+                    {tripUsers}
+                </ul>
             </div>
         )
     }
