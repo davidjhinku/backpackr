3d9544752e1513b469708a764945af54b77e4f95
David <davidjhinku@gmail.com>
2021-02-04 21:27:05 -0500

    Added user trips styline

--- frontend/src/components/user_trips/trip_details.jsx
+++ frontend/src/components/user_trips/trip_details.jsx
@@ -7,20 +7,21 @@ class TripDetails extends React.Component {
 
         return (
             <li className="trip-card">
-                <div className="trip-name">
-                    <h2><Link to={`trips/${trip._id}`} >{trip.tripName}</Link></h2>
-                </div>
+                <div>
+                    <div className="trip-name">
+                        <h2 className='trip-details-color'><Link to={`trips/${trip._id}`} >{trip.tripName}</Link></h2>
+                    </div>
 
-                <div className='trip-details'>
-                    <p>Destination: {trip.destination}</p>
-                    <p>Dates: {`${trip.startDate.slice(0,10)} - ${trip.endDate.slice(0,10)}`}</p>
-                    <div>
-                        <Link className="edit-trip" to={`trips/${trip._id}/edit`} trip={trip}>Edit This Trip</Link>
-                        <br/>
-                        <button className="delete-trip" onClick={() => this.props.deleteTrip(trip._id)}>Delete This Trip</button>
+                    <div className='trip-details'>
+                        <p className='trip-details-color'>Destination: {trip.destination}</p>
+                        <p className='trip-details-color'>Dates: {`${trip.startDate.slice(0,10)} - ${trip.endDate.slice(0,10)}`}</p>
                     </div>
                 </div>
-                <br/>
+
+                <div className='user-trip-links'>
+                    <Link className="edit-trip" to={`trips/${trip._id}/edit`} trip={trip}>Edit This Trip</Link>
+                    <button className="delete-trip" onClick={() => this.props.deleteTrip(trip._id)}>Delete This Trip</button>
+                </div>
             </li>
         );
     }
