bb8e73c89f7242d8c0810d903528d53be26e7fc9
David <davidjhinku@gmail.com>
2021-02-04 20:40:54 -0500

    Styled trip items

--- frontend/src/components/itinerary_item/itinerary_item_details.jsx
+++ frontend/src/components/itinerary_item/itinerary_item_details.jsx
@@ -4,9 +4,9 @@ const ItineraryItemDetails = ({item, deleteItem}) => {
     
     return (
             <li className='itinerary-item-list'>
-                {item.itemName}
-                <p>{item.description}</p>
-                <button onClick={() => deleteItem(item._id)}>Delete Item</button>
+                <p className='item-detail-side'>{item.itemName}</p>
+                <p className='item-detail-middle'>{item.description}</p>
+                <button className='item-detail-side' onClick={() => deleteItem(item._id)}>Delete Item</button>
             </li>
     )
 }
