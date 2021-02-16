92159dcaec0d95cefd611600db04ec08fb8fecef
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-02-16 10:39:27 -0600

    Add meta tags & fix comment delete

--- frontend/src/components/comments/comment_details.jsx
+++ frontend/src/components/comments/comment_details.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 class CommentDetails extends React.Component{
 
     render () {
-        debugger
         const deleteButton = (this.props.comment.author._id === this.props.currentUser.id) ? <button className="comment-delete-btn" onClick={() => this.props.deleteComment(this.props.comment._id)}>Delete Comment</button> : "";
 
         const messageClass = ((this.props.idx) % 2 === 0)? "message-bubble-1" : "message-bubble-2";
