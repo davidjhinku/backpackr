68dbac5862291c1203348b9d3a75c56f44653cf3
David <davidjhinku@gmail.com>
2021-02-15 21:23:41 -0500

    Users update automatically on add

--- frontend/src/actions/users_actions.js
+++ frontend/src/actions/users_actions.js
@@ -26,6 +26,7 @@ export const receiveErrors = errors => {
 export const addUserToTrip = user => dispatch => {
     return UsersAPIUtil.addUserToTrip(user)
         .then(user => {
+            debugger
             return dispatch(receiveUser(user))})
         .catch(err => {
             return dispatch(receiveErrors(err.response.data))})
--- frontend/src/reducers/comment_reducer.js
+++ frontend/src/reducers/comment_reducer.js
@@ -16,14 +16,15 @@ const CommentsReducer = (state = defaultState, action) => {
     let newState = Object.assign({}, state);
     switch (action.type) {
         case RECEIVE_A_TRIP:
+            // debugger
             const _newState = {}; // All previous comments need to be deleted.
             Object.values(action.trip.data)[0].comments
                 .forEach( comment => { _newState[comment._id] = comment });
             return _newState;
-
         case RECEIVE_ALL_COMMENTS:
             return action.comments
         case RECEIVE_COMMENT:
+            debugger
             return action.comment.data;
         case RECEIVE_NEW_COMMENT:
             newState[action.comment._id] = action.comment;
