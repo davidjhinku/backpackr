607d9539fd5f589fe4c0b3e9515152a83af64fec
audreyyun <96audreyyun@gmail.com>
2021-02-16 12:17:24 -0500

    Pin comments to bottom

--- frontend/src/components/comments/comments.jsx
+++ frontend/src/components/comments/comments.jsx
@@ -3,7 +3,26 @@ import CommentDetailsContainer from './comment_details_container';
 import CreateCommentContainer from './create_comment_container';
 
 class Comments extends React.Component {
-    
+    constructor(props) { 
+        super(props);
+        this.scrollToBottom = this.scrollToBottom.bind(this);
+        this.mRef = React.createRef();
+    }
+
+
+    componentDidMount() { 
+        this.scrollToBottom();
+    }
+
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom = () => { 
+        this.mRef.current.scrollIntoView({block:"end"})
+    }
+
+
     render() {
         const commentsList = this.props.comments.map((comment, idx) => { 
             return <CommentDetailsContainer idx={idx + 1} key={`err-${idx}`}comment={comment} />
@@ -12,11 +31,13 @@ class Comments extends React.Component {
 
         return (
             <div>
-                <ul className="comments-subcontainer">
-                    {commentsList}
-                    <br />
-                </ul>
-
+                <div className="comments-wrapper">
+                    <ul className="comments-subcontainer">
+                        {commentsList}
+                        <div ref={this.mRef}></div>
+                        <br />
+                    </ul>
+            </div>
                 <div className="comments-subcontainer-anchor"></div>
 
                 <div className="create-comment-container">
