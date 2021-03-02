912780a1fdc9e2551cb052524f01bb9b0db0c4fd
David <davidjhinku@gmail.com>
2021-03-02 13:49:09 -0500

    Added angellist links

--- frontend/src/components/nav/footer_items/david.js
+++ frontend/src/components/nav/footer_items/david.js
@@ -4,6 +4,7 @@ export default {
         // { name: "Personal Page", url: "url-here" },
         { name: "LinkedIn", url: "https://www.linkedin.com/in/davidjhinku/" },
         { name: "GitHub", url: "https://github.com/davidjhinku" },
+        { name: "AngelList", url: "https://angel.co/u/david-jhinku" },
         // { name: "Stack Overflow", url: "url-here" }
     ]
 }
--- frontend/src/components/nav/footer_items/henry.js
+++ frontend/src/components/nav/footer_items/henry.js
@@ -4,6 +4,7 @@ export default {
         // { name: "Personal Page", url: "url-here" },
         { name: "LinkedIn", url: "https://www.linkedin.com/in/zihaohuang/" },
         { name: "GitHub", url: "https://github.com/henryzihaohuang" },
+        { name: "AngelList", url: "https://angel.co/u/henry-zihao-huang" },
         // { name: "Stack Overflow", url: "url-here" }
     ]
 }
